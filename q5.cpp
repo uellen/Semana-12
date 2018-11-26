50cd979a770872f9b573c00d41187e5f0e68d4ae
Uellen Nicolete <31323969+uellen@users.noreply.github.com>
2018-11-26 14:51:17 -0300

    Update q5.cpp

--- q5.cpp
+++ q5.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 using namespace std;
+int const SMAX = 51;
+int const MAX = 20;
 
 int tamanho (char srt[]){
     int i = 0, cont =0;
@@ -13,7 +15,7 @@ int tamanho (char srt[]){
 void tratamento (char srt[]){
     int n = tamanho(srt);
     char srt2[n];
-    int i = 0, j =0;
+    int i = 0, j = 0, w = 0;
 
     while(srt[i] != '\0'){
         if(srt[i] != ' '){
@@ -22,66 +24,35 @@ void tratamento (char srt[]){
         }
         i++;
     }
-    j++;
     srt2[j] = '\0';
-    for(int w = 0; w < n; w++){
-        srt[w] = srt2[w];
-    }
+
+    srt = srt2;
+    cout<<srt<<"\n";
 }
 void ordenar (char str1[]){
     int n = tamanho(str1);
-    char str2[n];
-    int i = 0, j = 0, index = 0;
-    while(index != 25){
-        while(str1[i] != '\0'){
-            if((int)str1[i]-97 == index){
-                str2[j] = str1[i];
-                j++;
-                index++;
-            }
-            i++;
-        }
-        index++;
-    }
-    i = 0;
-    j = 0;
-    while(str2[j] != '\0'){
-        if(str2[j] != 0){
-            str1[i] = str2[j];
-            i++;
-        }
-        j++;
-    }
-    i++;
-    str1[i] = '\0';
-}
+    int i = 0, j = 0;
+    char aux;
 
-void validador (int v[]){
-    int i = 0, j = 0, cont = 1;
-    while (v[i] != '\0'){
-        while (v[j] != '\0'){
-            if(v[i] == v[j]){
-              cont++;
+    for(i; i < n-1; i++){
+        for(j = (i+1); j < n-1; j++){
+            if(str1[j] < str1[i]){
+                aux = str1[i];
+                str1[i] = str1[j];
+                str1[j] = aux;
             }
-            j++;
         }
-        cont = 0;
-        i++;
     }
+
+    cout<<str1<<endl;
 }
 
 void histograma (char str[], int vetor[]){
-    int i = 0, j = 0, cont = 0;
-    vetor[26] = {};
+    int i = 0, j = 0, cont = 1;
     tratamento(str);
     ordenar(str);
-    /*while(str[i] != '\0'){
-        cout<<str[i]<<" ";
-        i++;
-    }*/
 
     while(str[i] != '\0'){
-
         while(str[j] != '\0'){
             if(str[j] == str[i]){
               vetor[(int)str[i]-97] = cont++;
@@ -92,6 +63,7 @@ void histograma (char str[], int vetor[]){
         cont = 0;
         i++;
     }
+    cout<<str<<endl;
 }
 void exibir (int vet[], char str[]){
     int i =0;
@@ -102,13 +74,12 @@ void exibir (int vet[], char str[]){
 
 int main()
 {
-    char sa1[51];
-    int vet[26] = {};
+    char sa1[SMAX];
+    int vet[MAX] = {};
     cout<<"Digite uma frase.\n";
-    cin.getline(sa1,51);
+    cin.getline(sa1,SMAX);
 
     histograma(sa1,vet);
     exibir(vet,sa1);
     return 0;
 }
-
